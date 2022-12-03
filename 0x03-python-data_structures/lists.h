f555de977626ffacc96807dbf50a0029d1f852a2
Wenslaus <sifunawenslaus@gmail.com>
2022-12-03 04:43:04 -0800

    My task 14

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -28,6 +28,7 @@ def max_integer(my_list=[]):
 def divisible_by_2(my_list=[]):
 def delete_at(my_list=[], idx=0):
 int is_palindrome(listint_t **head);
+void print_python_list_info(PyObject *p);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
